a05c8c2d485c0591eb3545c7c29a3d32511240eb
Houria2003 <houriamorsli0@gmail.com>
2025-04-05 13:27:16 +0200

    base de données

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.sqlite3'
+
+
+def create_database():
+    db.create_all()
+    print('Database Created')
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your-secret-key-here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
 
     from .views import views
@@ -12,6 +24,10 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(admin, url_prefix='/')
+
+
+    with app.app_context():
+        create_database()
 
 
 
