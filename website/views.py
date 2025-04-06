6eb07ff0e99aaabd5468cfefa11ce1bcd13ceee7
Houria2003 <houriamorsli0@gmail.com>
2025-04-06 12:36:12 +0200

    Ignore database.sqlite3 and media files

--- website/views.py
+++ website/views.py
@@ -1,7 +1,48 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, flash, redirect, request, jsonify
+from .models import Product, Cart, Order
+from flask_login import login_required, current_user
+from . import db
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+
+    items = Product.query.filter_by(flash_sale=True)
+
+    return render_template('home.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+                           if current_user.is_authenticated else [])
+
+
+
+
+
+@views.route('/add-to-cart/<int:item_id>')
+@login_required
+def add_to_cart(item_id):
+    item_to_add = Product.query.get(item_id)
+    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
+    if item_exists:
+        try:
+            item_exists.quantity = item_exists.quantity + 1
+            db.session.commit()
+            flash(f' Quantity of { item_exists.product.product_name } has been updated')
+            return redirect(request.referrer)
+        except Exception as e:
+            print('Quantity not Updated', e)
+            flash(f'Quantity of { item_exists.product.product_name } not updated')
+            return redirect(request.referrer)
+
+    new_cart_item = Cart()
+    new_cart_item.quantity = 1
+    new_cart_item.product_link = item_to_add.id
+    new_cart_item.customer_link = current_user.id
+
+    try:
+        db.session.add(new_cart_item)
+        db.session.commit()
+        flash(f'{new_cart_item.product.product_name} added to cart')
+    except Exception as e:
+        print('Item not added to cart', e)
+        flash(f'{new_cart_item.product.product_name} has not been added to cart')
+
+    return redirect(request.referrer)
